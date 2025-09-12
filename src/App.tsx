450b91c24d16c8233c9c2adf9e295e5693d7b99c
HusseinMShamas <https://github.com/HusseinMShamas>
2025-09-12 15:39:46 +0300

    Header Component Creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import CalendarHeader from "@/Components/CalendarHeader.tsx";
+
 function App() {
-	return <div className="container m-auto mt-5"></div>;
+	return <div className="container m-auto mt-5"> <CalendarHeader /></div>;
 }
 
-export default App;
+export default App
