aaa7117e0b919a43b083da497a803e554004f57d
HusseinMShamas <https://github.com/HusseinMShamas>
2025-09-15 15:17:42 +0300

    Year View Container

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import CalendarHeader from "@/Components/calendar-header.tsx";
 import {CalendarContextProvider} from "@/context/calendar-context.tsx";
+import YearView from "@/Components/YearView.tsx";
 
 function App() {
     return (
         <CalendarContextProvider  >
             <div className="container m-auto mt-5">
             <CalendarHeader />
+                <YearView/>
             </div>
         </CalendarContextProvider>
     )
