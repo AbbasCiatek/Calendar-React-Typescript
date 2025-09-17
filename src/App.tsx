d680dca4db47996e64f6dbd5ad3ca99f16320b56
HusseinMShamas <https://github.com/HusseinMShamas>
2025-09-17 15:58:18 +0300

    Week View Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import CalendarHeader from "@/Components/calendar-header.tsx";
 import { CalendarContextProvider } from "@/context/calendar-context.tsx";
+import WeekView from "@/Components/WeekView.tsx";
 
 function App() {
 	return (
 		<CalendarContextProvider>
 			<div className="container m-auto mt-5">
 				<CalendarHeader />
+                <WeekView date={date}/>
 			</div>
 		</CalendarContextProvider>
 	);
 }
 
-export default App;
+export default App
