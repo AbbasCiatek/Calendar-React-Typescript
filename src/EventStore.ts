5d5feef40807cb6148333cf206d15ef4ca6bf3b4
HusseinMShamas <https://github.com/HusseinMShamas>
2025-09-19 14:34:28 +0300

    zod schema, zustand store and event type

--- src/EventStore.ts
+++ src/EventStore.ts
@@ -10,27 +10,24 @@ type EventsStore = {
 };
 
 const useEventStore = create<EventsStore>()(
-	persist(
-		(set) => ({
-			events: [],
-			addEvent: (event) =>
-				set((state) => ({
-					events: [...state.events, event],
-				})),
-			editEvent: (id, edited) =>
-				set((state) => ({
-					events: state.events.map((e) =>
-						e.id === id ? { ...e, ...edited } : e,
-					),
-				})),
+    persist(
+        (set)=>({
+            events : [],
+            addEvent : (event)=>
+                set((state)=>({
+                    events:[...state.events, event],
+                })),
+            editEvent: (id, edited) =>
+                set((state) => ({
+                    events: state.events.map((e) =>
+                    ),
+                })),
 
-			removeEvent: (id) =>
-				set((state) => ({
-					events: state.events.filter((e) => e.id !== id),
-				})),
-		}),
-		{ name: "Event-Storage" },
-	),
+            removeEvent: (id) =>
+                set((state) => ({
+                    events: state.events.filter((e) => e.id !== id),
+                })),
+        }),
 );
 
 export default useEventStore;
