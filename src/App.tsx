86810dfd3e3fee2ee601e4711249573a38d17d90
HusseinMShamas <https://github.com/HusseinMShamas>
2025-09-24 16:55:08 +0300

    event positioning enhancment

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import DayView from "@/Components/DayView.tsx";
 
 function App() {
-    const date = new Date();
+    const date = new Date(2025,8,23);
   return (
     <>
         <DayView date={date}/>
