8ea187b14338fdc60f3d05daecfd79ae66483890
HusseinMShamas <https://github.com/HusseinMShamas>
2025-09-25 11:33:16 +0300

    today design remove

--- src/Components/DayViewComponents/DayDisplay.tsx
+++ src/Components/DayViewComponents/DayDisplay.tsx
@@ -6,14 +6,13 @@ export default function DayDisplay({date}:{date:Date}){
     return(
         <div className="flex-1 border-l pt-2 pb-1 px-2">
         <div className="flex flex-col items-start text-xs font-medium  ">
-            <div className="flex flex-col items-center gap-1">
-            <span className={clsx("leading-none ",
-                isToday(date) ? "text-blue-500" : "text-muted-foreground")}>
+            <div className="flex items-center gap-2">
+            <span className="leading-none ">
                 {formatDate(date, "EE").toUpperCase()}
             </span>
-            <span className={clsx("flex  justify-center items-center size-10 rounded-full", isToday(date) ? ' bg-blue-500 text-white':'text-foreground')}>
-                <span className=" leading-none font-bold text-lg" >{formatDate(date, "d")}</span>
-                               </span>
+                <span className=" leading-none font-bold">
+                                      {formatDate(date, "d")}
+                                  </span>
             </div>
         </div>
         </div>
