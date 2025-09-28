9a43ca04d1d382da84b694c8079378e80dbb2f66
HusseinMShamas <https://github.com/HusseinMShamas>
2025-09-28 16:44:28 +0300

    Agenda view

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import CalendarHeader from "@/Components/calendar-header.tsx";
 import {CalendarContextProvider} from "@/context/calendar-context.tsx";
+import AgendaView from "@/Components/Agenda-View/AgendaView.tsx";
 
 function App() {
     return (
         <CalendarContextProvider  >
             <div className="container m-auto mt-5">
             <CalendarHeader />
+                <AgendaView/>
             </div>
         </CalendarContextProvider>
     )
