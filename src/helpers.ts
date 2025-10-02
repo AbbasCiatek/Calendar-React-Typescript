262a83af13173e66277e13ba4a3a4f9e5de4827d
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-02 11:53:56 +0300

    unifiying colors

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,19 +1,10 @@
 export const Hours = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
-export const backgroundFromColor = (colorSelected: string) => {
-    switch (colorSelected) {
-        case "red":
-            return "border-red-200 bg-red-100/50 text-red-700 hover:bg-red-100 dark:border-red-800 dark:bg-red-950 dark:text-red-300 dark:hover:bg-red-950";
-        case "green":
-            return "border-green-200 bg-green-100/50 text-green-700 hover:bg-green-100 dark:border-green-800 dark:bg-green-950 dark:text-green-300 dark:hover:bg-green-950";
-        case "yellow":
-            return "border-yellow-200 bg-yellow-100/50 text-yellow-700 hover:bg-yellow-100 dark:border-yellow-800 dark:bg-yellow-950 dark:text-yellow-300 dark:hover:bg-yellow-950";
-        case "blue":
-            return "border-blue-200 bg-blue-100/50 text-blue-700 hover:bg-blue-100 dark:border-blue-800 dark:bg-blue-950 dark:text-blue-300 dark:hover:bg-blue-950";
-        case "purple":
-            return  "border-purple-200 bg-purple-100/50 text-purple-700 hover:bg-purple-100 dark:border-purple-800 dark:bg-purple-950 dark:text-purple-300 dark:hover:bg-purple-950";
-        case "orange":
-            return "border-orange-200 bg-orange-100/50 text-orange-700 hover:bg-orange-100 dark:border-orange-800 dark:bg-orange-950 dark:text-orange-300 dark:hover:bg-orange-950";
-        default:
-            return "border-blue-200 bg-blue-100/50 text-blue-700 hover:bg-blue-100 dark:border-blue-800 dark:bg-blue-950 dark:text-blue-300 dark:hover:bg-blue-950"
-    }
-}
+export const colorMap: Record<string, string> = {
+    red: "bg-red-100 text-red-900 border border-red-400 ",
+    green: "bg-green-100 text-green-900 border border-green-400 ",
+    blue: "bg-blue-100 text-blue-900 border border-blue-400 ",
+    yellow: "bg-yellow-100 text-yellow-900 border border-yellow-400 ",
+    gray: "bg-gray-100 text-gray-900 border border-gray-400 ",
+    purple: "bg-purple-100 text-purple-900 border border-purple-400 ",
+    orange: "bg-orange-100 text-orange-900 border border-orange-400 ",
+};
