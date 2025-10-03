2a723f74342d6becdd90fa0963445f4f259d8838
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-03 14:44:18 +0300

    resolving rebase and design Conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import CalendarHeader from "@/Components/calendar-header.tsx";
-import {CalendarContextProvider} from "@/context/calendar-context.tsx";
-import WeekView from "@/Components/WeekView.tsx";
+import { CalendarContextProvider } from "@/context/calendar-context.tsx";
 
 function App() {
-    return (
-        <CalendarContextProvider  >
-            <div className="container m-auto mt-5">
-            <CalendarHeader />
-                <WeekView date={date}/>
-            </div>
-        </CalendarContextProvider>
-    )
+	return (
+		<CalendarContextProvider>
+			<div className="container m-auto mt-5">
+				<CalendarHeader />
+			</div>
+		</CalendarContextProvider>
+	);
 }
 
-export default App
+export default App;
