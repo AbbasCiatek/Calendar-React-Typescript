85e4fee1e10e4635bea4666c71215bee51f9ca05
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-03 10:42:51 +0300

    naming + conflect solving

--- src/Components/CalendarHeader.tsx
+++ src/Components/CalendarHeader.tsx
@@ -2,6 +2,7 @@ import  {AddEventDialog} from "@/Components/HeadersComponent/AddEventButton.tsx"
 
 
 export default function CalendarHeader() {
+
     return (
         <div className="flex items-center border p-5 m-5 rounded-t-2xl font-bold">
             <AddEventDialog/>
