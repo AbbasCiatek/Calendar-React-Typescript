fa07b0ffb13a13fb23b36f304d2ad7722d9cd774
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-03 13:55:57 +0300

    resolving Conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import CalendarHeader from "@/Components/calendar-header.tsx";
-import {CalendarContextProvider} from "@/context/calendar-context.tsx";
-import YearView from "@/Components/YearView.tsx";
+import YearView from "@/Components/year-view.tsx";
+import { CalendarContextProvider } from "@/context/calendar-context.tsx";
 
 function App() {
-    return (
-        <CalendarContextProvider  >
-            <div className="container m-auto mt-5">
-            <CalendarHeader />
-                <YearView/>
-            </div>
-        </CalendarContextProvider>
-    )
+	return (
+		<CalendarContextProvider>
+			<div className="container m-auto mt-5">
+				<CalendarHeader />
+				<YearView />
+			</div>
+		</CalendarContextProvider>
+	);
 }
 
-export default App
+export default App;
