22de1c8a28dc49a496afbb41bc12d22f536eb1a9
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-03 15:59:22 +0300

    resolving rebase and design Conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
+import AgendaView from "@/Components/agenda-view.tsx";
 import CalendarHeader from "@/Components/calendar-header.tsx";
-import {CalendarContextProvider} from "@/context/calendar-context.tsx";
-import AgendaView from "@/Components/Agenda-View/AgendaView.tsx";
+import { CalendarContextProvider } from "@/context/calendar-context.tsx";
 
 function App() {
-    return (
-        <CalendarContextProvider  >
-            <div className="container m-auto mt-5">
-            <CalendarHeader />
-                <AgendaView/>
-            </div>
-        </CalendarContextProvider>
-    )
+	return (
+		<CalendarContextProvider>
+			<div className="container m-auto mt-5">
+				<CalendarHeader />
+				<AgendaView />
+			</div>
+		</CalendarContextProvider>
+	);
 }
 
-export default App
+export default App;
--- src/EventStore.ts
+++ src/EventStore.ts
@@ -1,52 +1,57 @@
-import { persist } from "zustand/middleware";
+import { areIntervalsOverlapping } from "date-fns";
 import { create } from "zustand";
-import {areIntervalsOverlapping} from "date-fns";
+import { persist } from "zustand/middleware";
+import type { Event } from "@/types";
 
 type EventsStore = {
 	events: Event[];
+	getEventsByDateRange: (startDate: Date, endDate: Date) => Event[];
 	addEvent: (event: Event) => void;
 	editEvent: (id: string, edited: Partial<Event>) => void;
 	removeEvent: (id: string) => void;
-    getEventByDateRange:(startDate:Date, endDate:Date) => Event[];
 };
 
 const useEventStore = create<EventsStore>()(
-    persist(
-        (set,get)=>({
-            events : [],
-            getEventsByDateRange: (startDate:Date, endDate:Date) => {
-                if (!startDate || !endDate) return get().events;
+	persist(
+		(set, get) => ({
+			events: [],
+			getEventsByDateRange: (startDate: Date, endDate: Date) => {
+				if (!startDate || !endDate) return get().events;
+				else
+					return get().events.filter((event) => {
+						if (
+							areIntervalsOverlapping(
+								{
+									start: event.startDate,
+									end: event.endDate,
+								},
+								{
+									start: startDate,
+									end: endDate,
+								},
+							)
+						)
+							return event;
+					});
+			},
+			addEvent: (event) =>
+				set((state) => ({
+					events: [...state.events, event],
+				})),
+			editEvent: (id, edited) =>
+				set((state) => ({
+					events: state.events.map((e) =>
+						e.id === id ? { ...e, ...edited } : e,
+					),
+				})),
 
-                return get().events.filter((event) => {
-                    if(areIntervalsOverlapping({
-                       start:event.startDate,
-                        end:event.endDate,
-                    },{
-                        start:startDate,
-                        end:endDate,
-                    }))
-                    return (event);
-                });
-            },
-
-            addEvent : (event)=>
-                set((state)=>({
-                    events:[...state.events, event],
-                })),
-            editEvent: (id, edited) =>
-                set((state) => ({
-                    events: state.events.map((e) =>
-                        e.id === id ? { ...e, ...edited } : e
-                    ),
-                })),
-
-            removeEvent: (id) =>
-                set((state) => ({
-                    events: state.events.filter((e) => e.id !== id),
-                })),
-        }),
-        { name: "Event-Storage" }
-    )
+			removeEvent: (id) =>
+				set((state) => ({
+					events: state.events.filter((e) => e.id !== id),
+				})),
+		}),
+		{ name: "Event-Storage" },
+	),
 );
 
 export default useEventStore;
