77ef539ca8450d48e29528a740ace8a596032884
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-07 10:40:00 +0300

    kebab naming

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import DndProviderWrapper from "@/dnd/DndProviderWrapper.tsx";
+import DndProviderWrapper from "@/dnd/dnd-provider-wrapper.tsx";
 
 function App() {
-
-  return (
-    <DndProviderWrapper>
-        <div className="container m-auto mt-5"></div>;
-    </DndProviderWrapper>
-  )
+	return (
+		<DndProviderWrapper>
+			<div className="container m-auto mt-5"></div>;
+		</DndProviderWrapper>
+	);
 }
 
-export default App
+export default App;
